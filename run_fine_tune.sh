82d8d6235d36755233cd5526109990a95f805bca
qklent <maksimsilenko8@gmail.com>
2025-11-09 09:49:04 +0300

    fix typo in run script

--- run_fine_tune.sh
+++ run_fine_tune.sh
@@ -2,7 +2,7 @@ python3 -m f5_tts.train.train_mask_fill \
       --dataset_path /home/qklent/programming/speech_disorder_correction/mlm/data_preparation/data/prod_checkpoint \
       --exp_name F5TTS_v1_Base \
       --finetune \
-      --pretrain hf://ESpeech/ESpeech-TTS-1_RL-V2/ espeech_tts_rlv2.pt \
+      --pretrain hf://ESpeech/ESpeech-TTS-1_RL-V2/espeech_tts_rlv2.pt \
       --logger wandb \
       --log_samples \
       --batch_size_per_gpu 3200 \
